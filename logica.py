062c1f2c87f35cca2e495fbb4de45732ddd39f4f
leslenaaa <vh3081001@gmail.com>
2025-02-27 15:29:48 -0600

    se creo un nuevo recibo

---
+++ logica.py
@@ -0,0 +1,14 @@
+#un cajero automatico
+
+usuario = list()
+
+usuario.append("leslena") #string str
+usuario.append("1234") #strng str
+usuario.append(1000) #int
+
+recibo = list()
+recibo.append(["123" , 600]) 
+recibo.append(["124" , 845]) 
+recibo.append(["125" , 67]) 
+recibo.append(["126" , 600]) 
+recibo.append(["127" , 60]) 
